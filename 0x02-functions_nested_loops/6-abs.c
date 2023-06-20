ef54a446d79ba72ea01e77541c2d578fdd83a000
ayoubweb <ayoubweb02@gmail.com>
2023-06-20 23:10:14 +0100

    02

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - func
+ * @ch:int
+ * Return: 0
+ */
+int _abs(int ch)
+{
+	if (ch > 0 || ch == 0)
+	{
+	return (ch);
+	}
+	else
+	return (ch * -1);
+}
