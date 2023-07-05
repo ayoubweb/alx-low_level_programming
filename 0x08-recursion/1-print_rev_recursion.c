ca181681f08ba23708104efc4c48f69e2422dde3
ayoubweb <ayoubweb02@gmail.com>
2023-07-06 00:13:31 +0100

    08

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _print_rev_recurson - prints strng in reverse
- * @s: ponter to the strng
- * Return: vod
+ * @t: ponter to th strng
+ * Return: void
  */
-void _print_rev_recursion(char *s)
+void _print_rev_recursion(char *t)
 {
-	if (*s > '\0')
+	if (*t > '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		_print_rev_recursion(t + 1);
+		_putchar(*t);
 	}
 
 }
