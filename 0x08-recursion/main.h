ba4c6e340fe18bb23ad1f34911caa10c1340dadf
ayoubweb <ayoubweb02@gmail.com>
2023-07-06 01:32:44 +0100

    08

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,7 +5,7 @@ int _strlen_recursion(char *t);
 int factorial(int r);
 int _pow_recursion(int a, int b);
 int square(int m, int v);
-int _sqrt_recursion(int num);
+int _sqrt_recursion(int m);
 int check_prime(int num, int rsp);
 int is_prime_number(int n);
 int last_index(char *s);
